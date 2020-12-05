867b4e1064d3a3f21ec0666571fe3915b1f502de
Cosmo326 <acosentino326@gmail.com>
2020-12-05 11:10:13 -0500

    day 2 2015 and day 5 2020

--- Year/_2015/_2/Day2.go
+++ Year/_2015/_2/Day2.go
@@ -1,13 +1,62 @@
 package _2
 
+import (
+	"AdventOfCode/Importer"
+	"fmt"
+	"sort"
+	"strings"
+)
+
 const YearNumber = 2015
 const DayNumber = 2
 const FileName = "input.txt"
 
 func GetResult1 () (string, error) {
-	return "Not Completed", nil
+	input := Importer.ReadDayInput(YearNumber, DayNumber, FileName)
+	inputArr := strings.Split(input, "\n")
+	total := 0
+
+	for _,v := range inputArr{
+		var l, w, h int
+		_, scanErr := fmt.Sscanf(v, "%dx%dx%d", &l, &w, &h)
+
+		if scanErr != nil {
+			panic(scanErr)
+		}
+
+		total += 2*l*w + 2*l*h + 2*h*w + GetMinArea(l,h,w)
+	}
+
+	return fmt.Sprintf("%d", total), nil
+}
+
+func GetMinArea(l,w,h int) int {
+	if l*w <= l*h && l*w <= h*w {
+		return l*w
+	} else if l*h <= l*w && l*h <= h*w {
+		return l*h
+	} else {
+		return h*w
+	}
 }
 
 func GetResult2 () (string, error) {
-	return "Not Completed", nil
+	input := Importer.ReadDayInput(YearNumber, DayNumber, FileName)
+	inputArr := strings.Split(input, "\n")
+	var total int = 0
+
+	for _,v := range inputArr{
+		var dims = []int {0,0,0}
+		_, scanErr := fmt.Sscanf(v, "%dx%dx%d", &dims[0], &dims[1], &dims[2])
+
+		if scanErr != nil {
+			panic(scanErr)
+		}
+
+		sort.Ints(dims)
+
+		total += 2 * dims[0] + 2 * dims[1] + dims[0] * dims[1] * dims[2]
+	}
+
+	return fmt.Sprintf("%d", total), nil
 }
--- Year/_2020/_5/Day5.go
+++ Year/_2020/_5/Day5.go
@@ -1,13 +1,82 @@
 package _5
 
+import (
+	"AdventOfCode/Importer"
+	"fmt"
+	"math"
+	"sort"
+	"strings"
+)
+
 const YearNumber = 2020
 const DayNumber = 5
 const FileName = "input.txt"
 
 func GetResult1 () (string, error){
-	return "Not Completed", nil
+	input := Importer.ReadDayInput(YearNumber, DayNumber, FileName)
+	inputArr := strings.Split(input, "\n")
+	highestSeat := 0
+
+	for _,v := range inputArr {
+		var rowInst, colInst string
+		_, scanErr := fmt.Sscanf(v, "%7s%3s", &rowInst, &colInst)
+
+		if scanErr != nil {
+			panic(scanErr)
+		}
+		row := TraverseTree(rowInst)
+		col := TraverseTree(colInst)
+
+		seat := row * 8 + col
+
+		if seat > highestSeat {
+			highestSeat = seat
+		}
+	}
+
+	return fmt.Sprintf("%d", highestSeat), nil
 }
 
 func GetResult2 () (string, error){
-	return "Not Completed", nil
+	input := Importer.ReadDayInput(YearNumber, DayNumber, FileName)
+	inputArr := strings.Split(input, "\n")
+	var seats []int
+
+	for _,v := range inputArr {
+		var rowInst, colInst string
+		_, scanErr := fmt.Sscanf(v, "%7s%3s", &rowInst, &colInst)
+
+		if scanErr != nil {
+			panic(scanErr)
+		}
+		row := TraverseTree(rowInst)
+		col := TraverseTree(colInst)
+
+		seat := row * 8 + col
+
+		seats = append(seats, seat)
+	}
+
+	sort.Ints(seats)
+	for s := 0; s < len(seats) - 1; s++{
+		if seats[s] + 2 == seats[s + 1]{
+			return fmt.Sprintf("%d", seats[s] + 1), nil
+		}
+	}
+
+	return "Seat Doesn't Exist", nil
+}
+
+func TraverseTree(instructions string) int{
+	position := 0
+	half := int(math.Pow(2, float64(len(instructions)))) / 2
+
+	for _, i := range instructions {
+		if string(i) == "B" || string(i) == "R" {
+			position += half
+		}
+		half /= 2
+	}
+
+	return position
 }
