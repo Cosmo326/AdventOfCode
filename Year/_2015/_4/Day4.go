dd0779ef6834009d03f08fe5cb8e41f282070507
Cosmo326 <acosentino326@gmail.com>
2020-12-05 17:12:39 -0500

    2015 day 4

--- Year/_2015/_4/Day4.go
+++ Year/_2015/_4/Day4.go
@@ -1,13 +1,33 @@
 package _4
 
-const YearNumber = 2015
-const DayNumber = 4
-const FileName = "input.txt"
+import (
+	"crypto/md5"
+	"fmt"
+	"regexp"
+)
+
+const input = "bgvyzdsv"
 
 func GetResult1 () (string, error){
-	return "Not Completed", nil
+	regex, _ := regexp.Compile("^00000.+")
+
+	return fmt.Sprintf("%d", FindCompletionInt(regex)), nil
 }
 
 func GetResult2 () (string, error){
-	return "Not Completed", nil
+	regex, _ := regexp.Compile("^000000.+")
+
+	return fmt.Sprintf("%d", FindCompletionInt(regex)), nil
+}
+
+func FindCompletionInt(regex *regexp.Regexp) int{
+	i:=1
+	for {
+		data := []byte(fmt.Sprintf("%s%d", input, i))
+		hex := fmt.Sprintf("%x", md5.Sum(data))
+		if regex.MatchString(hex) {
+			return i
+		}
+		i++
+	}
 }
