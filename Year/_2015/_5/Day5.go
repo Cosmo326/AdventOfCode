ce0fd7cd724dfad6ae614f58f29d29a2ac790f3c
Cosmo326 <acosentino326@gmail.com>
2020-12-06 21:15:21 -0500

    2020 day 6 completion and ugly completion of 2015 day 5.  May try to refactor later

--- Year/_2015/_5/Day5.go
+++ Year/_2015/_5/Day5.go
@@ -19,13 +19,10 @@ func GetResult1 () (string, error){
 
 	for _,v := range inputArr{
 		if CheckBadStrings(1, v) {
-			//println(v)
 			naughty++
 		} else if CheckDoubles(1, v) && CheckVowels(3, v) {
-			//println(v)
 			nice++
 		} else {
-			println(v)
 			naughty++
 		}
 	}
@@ -34,7 +31,20 @@ func GetResult1 () (string, error){
 }
 
 func GetResult2 () (string, error){
-	return "Not Completed", nil
+	input := Importer.ReadDayInput(YearNumber, DayNumber, FileName)
+	inputArr := strings.Split(input, "\n")
+	naughty := 0
+	nice := 0
+
+	for _,v := range inputArr{
+		if CheckDuplicatePairs(1, v) && CheckSingleSeparatedDoubles(1, v) {
+			nice++
+		} else {
+			naughty++
+		}
+	}
+
+	return fmt.Sprintf("%d", nice), nil
 }
 
 func CheckVowels(count int, line string) bool{
@@ -47,6 +57,29 @@ func CheckDoubles(count int, line string) bool{
 	regex, _ := regexp.Compile("aa|bb|cc|dd|ee|ff|gg|hh|ii|jj|kk|ll|mm|nn|oo|pp|qq|rr|ss|tt|uu|vv|ww|xx|yy|zz")
 	matches := regex.FindAllString(line, -1)
 	return len(matches) >= count
+}
+
+func CheckSingleSeparatedDoubles(count int, line string) bool{
+	regex, _ := regexp.Compile("a.a|b.b|c.c|d.d|e.e|f.f|g.g|h.h|i.i|j.j|k.k|l.l|m.m|n.n|o.o|p.p|q.q|r.r|s.s|t.t|u.u|v.v|w.w|x.x|y.y|z.z")
+	matches := regex.FindAllString(line, -1)
+	return len(matches) >= count
+}
+
+func CheckDuplicatePairs(count int, line string) bool{
+	subs := make(map[string]int)
+	triples := CountTriples(line)
+
+	for i:=0; i < len(line) - 1; i++{
+		subs[line[i:i+2]]++
+	}
+
+	return len(line) - 1 - (len(subs) + triples) >= count
+}
+
+func CountTriples(line string) int{
+	regex, _ := regexp.Compile("aaa|bbb|ccc|ddd|eee|fff|ggg|hhh|iii|jjj|kkk|lll|mmm|nnn|ooo|ppp|qqq|rrr|sss|ttt|uuu|vvv|www|xxx|yyy|zzz")
+	matches := regex.FindAllString(line, -1)
+	return len(matches)
 }
 
 func CheckBadStrings(count int, line string) bool{
--- Year/_2020/_6/Day6.go
+++ Year/_2020/_6/Day6.go
@@ -1,13 +1,68 @@
 package _6
 
+import (
+	"AdventOfCode/Importer"
+	"fmt"
+	"strings"
+)
+
 const YearNumber = 2020
 const DayNumber = 6
 const FileName = "input.txt"
 
 func GetResult1 () (string, error){
-	return "Not Completed", nil
+	input := Importer.ReadDayInput(YearNumber,DayNumber,FileName)
+	groups := strings.Split(input, "\n\n")
+	total := 0
+
+	for _,g := range groups{
+		group := strings.Split(g, "\n")
+		total += GetGroupAnswerCount(group)
+	}
+
+	return fmt.Sprintf("%d", total), nil
 }
 
 func GetResult2 () (string, error){
-	return "Not Completed", nil
+	input := Importer.ReadDayInput(YearNumber,DayNumber,FileName)
+	groups := strings.Split(input, "\n\n")
+	total := 0
+
+	for _,g := range groups{
+		group := strings.Split(g, "\n")
+		total += GetGroupFullAnswerCount(group)
+	}
+
+	return fmt.Sprintf("%d", total), nil
+}
+
+func GetGroupAnswerCount(group []string)int{
+	questions := make(map[string]int)
+
+	for _,person := range group{
+		for _,q := range person{
+			questions[string(q)]++
+		}
+	}
+
+	return len(questions)
+}
+
+func GetGroupFullAnswerCount(group []string)int{
+	questions := make(map[string]int)
+
+	for _,person := range group{
+		for _,q := range person{
+			questions[string(q)]++
+		}
+	}
+
+	allCorrect := 0
+	for _, question := range questions{
+		if question == len(group){
+			allCorrect++
+		}
+	}
+
+	return allCorrect
 }
